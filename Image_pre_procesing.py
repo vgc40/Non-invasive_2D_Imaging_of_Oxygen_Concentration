b9919bbee4afc85a1abfe6aa220fdb628f95d68a
Vanessa Garayburu-Caruso <vanegar40@gmail.com>
2021-06-14 20:01:25 -0700

    added python script

---
+++ Image_pre_procesing.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 14 12:33:09 2021
+
+@author: gara009
+"""
+
+import os
+import cv2
+
+
+
+mypath='//pnl/projects/SBR_SFA/RC4/VGC/Images'
+outpath='//pnl/projects/SBR_SFA/RC4/VGC/Images/output3'
+
+if not os.path.exists(outpath):
+    os.makedirs(outpath)
+
+
+
+filenamelist=[]
+
+
+
+for file in os.listdir(mypath):
+    if file.endswith(".tif"):
+        filenamelist.append(file)
+
+
+for file in filenamelist:
+    temp=cv2.imread(os.path.join(mypath,file),-1)
+    cv2.imwrite(os.path.join(outpath,file),temp)
